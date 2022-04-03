5eac0f6c893b7f8e22747f6d8e310784ef0e15c4
amachkel <amachkel@gmail.com>
2022-04-03 18:15:14 -0400

    user's posts render to dashboard and can be deleted

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,17 +1,17 @@
-// const delButtonHandler = async (event) => {
-//   if (event.target.hasAttribute('data-id')) {
-//     const id = event.target.getAttribute('data-id');
+const delButtonHandler = async (event) => {
+  if (event.target.hasAttribute('data-id')) {
+    const id = event.target.getAttribute('data-id');
+    console.log('delete btn function called');
+    const response = await fetch(`/api/posts/${id}`, {
+      method: 'DELETE',
+    });
 
-//     const response = await fetch(`/api/posts/${id}`, {
-//       method: 'DELETE',
-//     });
+    if (response.ok) {
+      document.location.replace('/dashboard');
+    } else {
+      alert('Failed to delete post');
+    }
+  }
+};
 
-//     if (response.ok) {
-//       document.location.replace('/dashboard');
-//     } else {
-//       alert('Failed to delete post');
-//     }
-//   }
-// };
-
-// document.querySelector('.delete').addEventListener('click', delButtonHandler);
+document.querySelector('.delete').addEventListener('click', delButtonHandler);
