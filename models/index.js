fd5b30d985352d035698d235d9f6e5d989ad54a7
amachkel <amachkel@gmail.com>
2022-04-09 13:23:12 -0400

    fixed delete post bug

--- models/index.js
+++ models/index.js
@@ -4,24 +4,27 @@ const Comment = require('./Comment');
 
 User.hasMany(Post, {
   foreignKey: 'user_id',
-  onDelete: 'SET NULL',
+  onDelete: 'CASCADE',
 });
 
 Post.belongsTo(User, {
   foreignKey: 'user_id',
+  onDelete: 'CASCADE',
 });
 
 Comment.belongsTo(User, {
   foreignKey: 'user_id',
+  onDelete: 'CASCADE',
 });
 
 User.hasMany(Comment, {
   foreignKey: 'user_id',
-  onDelete: 'SET NULL',
+  onDelete: 'CASCADE',
 });
 
 Comment.belongsTo(Post, {
   foreignKey: 'post_id',
+  onDelete: 'CASCADE',
 });
 
 Post.hasMany(Comment, {
